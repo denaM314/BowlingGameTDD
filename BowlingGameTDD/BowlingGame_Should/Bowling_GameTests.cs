988187bd22b0e56b0a4c51346ec384c1ae20f636
denaM314 <38193649+denaM314@users.noreply.github.com>
2020-08-02 14:12:31 -0500

    Clean up test

--- BowlingGameTDD/BowlingGame_Should/Bowling_GameTests.cs
+++ BowlingGameTDD/BowlingGame_Should/Bowling_GameTests.cs
@@ -5,15 +5,16 @@ namespace BowlingGame_Should
 {
     public class Tests
     {
+        private Game game;
         [SetUp]
         public void Setup()
         {
+            game = new Game();
         }
 
         [Test]
         public void Gutterball_does_not_increase_score()
         {
-            var game = new Game();
             game.Roll(0);
             Assert.AreEqual(game.Score(), 0);
         }
